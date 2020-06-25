66b07941cc3345e06f0d4609a916111fd99fcc25
unknown <rishavmittal2903@gmail.com>
2020-06-25 11:29:20 +0530

    Updates

---
+++ precache-manifest.b289d55c46988d36022adbd371c404c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53f0ce53bd3b88090077d4012b97e8f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8c1df48ddbfb51d41837",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/Portfolio/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1df48ddbfb51d41837",
+    "url": "/Portfolio/static/js/main.a925d1f0.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
